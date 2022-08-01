7d325f6c68e72311a7a9bbd6ee9e524e4dd3cc9a
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-08-02 00:58:59 +0200

    initial router v6  +  components setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+
+import Actors from './Actors/Actors';
+import Movies from './Movies/Movies';
+import MovieInfo from './MovieInfo/MovieInfo';
+import NavigationBar from './NavigationBar/NavigationBar';
+import Profile from './Profile/Profile';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <CssBaseline />
+      <NavigationBar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/movie/:id" element={<MovieInfo />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
     </div>
   );
 }
