338e01c82f73b24c4c433446c4ceece9d136c2dd
Kamil Ertekin <kertekin19@epoka.edu.al>
2022-08-17 01:42:10 +0200

    Sign out feature on Profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { useSelector } from 'react-redux';
+import { Typography, Button, Box } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
 import { userSelector } from '../../features/auth';
 
 function Profile() {
+  const favoriteMovies = [];
   const { user } = useSelector(userSelector);
-  console.log(user);
-  return <div>Profile - {user.username}</div>;
+  const logout = () => {
+    localStorage.clear();
+
+    // reload the page
+    window.location.href = '/';
+  };
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Sign Out & <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length ? (
+        <Typography variant="h5">Add favorites or watch movies</Typography>
+      ) : (
+        <Box>Favorite Movies</Box>
+      )}
+    </Box>
+  );
 }
 
 export default Profile;
